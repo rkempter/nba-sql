5cf63a7e13966c6f850cf1903715b31f3f7c3a85
Renato Kempter <renato.kempter@gmail.com>
2012-04-20 11:30:02 +0200

    Header menu filed which is gonna be included in all files (top-navigation)

---
+++ _header.php
@@ -0,0 +1,36 @@
+<div class="navbar navbar-fixed-top">
+    <div class="navbar-inner">
+      <div class="container">
+        <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </a>
+        <a class="brand" href="./index.html">NBA Project</a>
+        <div class="nav-collapse">
+          <ul class="nav">
+            <li class="active">
+              <a href="./index.php">Home</a>
+            </li>
+            <li class="">
+              <a href="search.html">Search for Keyword</a>
+            </li>
+            <li class="">
+              <a href="insert_data.html">Insert elements</a>
+            </li>
+            <li class="dropdown">
+              <a class="dropdown-toggle" data-toggle="dropdown" href="#">Deliverable 2 <b class="caret"></b></a>
+              <ul class="dropdown-menu">
+                <li><a href="query_a.html">Query a</a></li>
+                <li><a href="query_b.html">Query b</a></li>
+                <li><a href="query_c.html">Query c</a></li>
+                <li><a href="query_d.html">Query d</a></li>
+                <li><a href="query_e.html">Query e</a></li>
+                <li><a href="query_f.html">Query f</a></li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
